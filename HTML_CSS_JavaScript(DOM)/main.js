28251d4264aea61066c9b8648da0e99df9e715f7
ifi2a <ifi2a@mtmail.mtsu.edu>
2017-11-21 22:24:28 -0600

    JavaScript project to show some of my work
    JavaScript projects using DOM

---
+++ HTML_CSS_JavaScript(DOM)/main.js
@@ -0,0 +1,13 @@
+﻿function main() {
+    $('.skillset').hide();
+    $('.skillset').fadeIn(1000);
+
+    $('.projects').hide();
+    $('.projects-button').on('click', function () {
+        //$('.projects').toggle();
+        $(this).next().slideToggle(400);
+        $(this).toggleClass('active');
+        $(this).text('projects Viewed');
+    });
+}
+$(document).ready(main);
